af133d6331e2a14feef58c5e157883d8b0256e0e
Dipanshu Pandey <dps.coders@gmail.com>
2024-09-14 21:44:20 +0530

    Redux learning initialized

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <p className='text-4xl font-bold text-red-500'>Hii there how are you, we are learning redux toolkit</p>  
+    </div>
+  )
+}
