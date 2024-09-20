7116a4a215a9feb28cb94e0a90607c22b0fcdd8c
Dipanshu Pandey <dps.coders@gmail.com>
2024-09-20 13:33:27 +0530

    Action payload added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import 'react-redux'
 import { useDispatch, useSelector } from 'react-redux'
-import { increment } from './store/reducer/counterSlice'
+import { addByValue, decrement, increment } from './store/reducer/counterSlice'
 
 export default function App() {
   const dispatch=useDispatch()
   const {value} = useSelector(state=>state.counter)
   console.log(value)
+
+
   return (
     <div className=''>
       <p className='text-4xl font-bold text-red-500'>Hii there how are you, we are learning redux toolkit</p>  
       <p className='text-3xl text-center mt-8'>Counter: {value}</p>
+
       <button onClick={()=>dispatch(increment())} className='mx-auto mt-4 bg-red-400 p-2 block border'>Increment</button>
+      <button onClick={()=>dispatch(decrement())} className='mx-auto mt-4 bg-green-400 p-2 block border'>Decrement</button>
+      <button onClick={()=>dispatch(addByValue(5))} className='mx-auto mt-4 bg-blue-400 p-2 block border'>Add by 5</button>
+    
     </div>
   )
 }
--- src/store/reducer/counterSlice.jsx
+++ src/store/reducer/counterSlice.jsx
@@ -2,22 +2,28 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState={
     value:0,
+    naam:'Alex'
 }
 
 export const counterSlice = createSlice({
-    name:'counter',
+    name:'app',
     initialState,
 
     reducers:{
         increment:(state)=>{
             state.value+=1;
+            state.naam.concat('a')
+            console.log(state.naam)
         },
         decrement:(state)=>{
             state.value-=1;
         },
+        addByValue:(state,action)=>{
+            state.value+=action.payload
+        }
     }
 })
 
-export const { increment, decrement } = counterSlice.actions
+export const { increment, decrement, addByValue } = counterSlice.actions
 
 export default counterSlice.reducer
