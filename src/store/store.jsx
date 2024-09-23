c70db67d5a29bbe7c0c7577dc60219de9d38cefa
Dipanshu Pandey <dps.coders@gmail.com>
2024-09-24 00:26:31 +0530

    Users added

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,8 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './reducer/counterSlice'
 
 export const store = configureStore({
   reducer: {
-    counter:counterReducer
+
   },
 })
