dca02a90edd2e503a4419fa8823253228334b26e
Dipanshu Pandey <dps.coders@gmail.com>
2024-09-24 14:07:35 +0530

    Users moved in store

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import  UserSlice  from './reducer/UserSlice'
 export const store = configureStore({
   reducer: {
-
+    UserSlice:UserSlice
   },
 })
